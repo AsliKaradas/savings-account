9a522cc7aefec930a0d3b59bb7e12e21bf33811e
Asli Karadas <144810131+AsliKaradas@users.noreply.github.com>
2024-03-22 00:02:30 +0100

    Checks users' information

--- run.py
+++ run.py
@@ -17,3 +17,19 @@ usernames = ['savings1', 'savings2', 'savings3', 'savings4']
 passwords = ['0001', '0002', '0003', '0004']
 amounts = [10000, 20000, 30000, 4000]
 count = 0
+
+# Checks users' information 
+while True:
+    user = input('\nPlease Enter Your  Username: ')
+    user = user.lower()
+
+    if user in usernames:
+        if user == usernames[0]:
+            n = 0
+        elif user == usernames[1]:
+            n = 1
+        else:
+            n = 2
+        break
+    else:
+        print('\n\n     Invalid Username!!')
